e36047155cfbd899721271a3c827c1cdcb21846a
GreeKpl <grkalk@gmail.com>
2015-10-30 14:10:40 +0100

    working SWI-prolog example

--- src/pl/edu/agh/se/Main.java
+++ src/pl/edu/agh/se/Main.java
@@ -1,53 +1,82 @@
 package pl.edu.agh.se;
 
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
 
 import jpl.Atom;
+import jpl.Compound;
 import jpl.Query;
 import jpl.Term;
-
-import javax.swing.*;
-import java.awt.*;
+import jpl.Util;
 
 public class Main extends JFrame {
 
-    private JTextArea topDescription;
-    private JButton yesButton;
-    private JButton noButton;
+	private JTextArea topDescription;
+	private JButton yesButton;
+	private JButton noButton;
 
-    public Main() {
-        initializeGUI();
+	public Main() {
+		initializeGUI();
 
-        jpl.Query q1 = new Query("consult",
-                new Term[]{new Atom("cos.pl")});
-        q1.oneSolution();
+		Query q1 = new Query("consult", new Term[] { new Atom("cos.pl") });
 
-    }
+		System.out.println("loaded successfully: " + q1.hasSolution());
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            Main main = new Main();
-            main.setVisible(true);
-        });
-    }
+		Query query = new Query(new Compound("elem", new Term[] {
+				new Atom("a"),
+				Util.termArrayToList(new Term[] { atom("a"), atom("b"),
+						atom("c") }) }));
+		topDescription.append("elem('f', ['a', 'b', 'c']");
 
-    private void initializeGUI() {
-        Container pane = getContentPane();
-        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		System.out.println(query.hasSolution());
 
-        setTitle("Java Prolog");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
 
-        topDescription = new JTextArea("Wybierz");
-        pane.add(topDescription);
-        yesButton = new JButton("TAK");
-        pane.add(yesButton);
-        noButton = new JButton("NIE");
-        pane.add(noButton);
+	private static Atom atom(String value) {
+		return new Atom(value);
+	}
 
-        JButton quitButton = new JButton("Koniec");
-        quitButton.addActionListener(event -> System.exit(0));
-        pane.add(quitButton);
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Main main = new Main();
+				main.setVisible(true);
+			}
+		});
+	}
+
+	private void initializeGUI() {
+		Container pane = getContentPane();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
+		setTitle("Java Prolog");
+		setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+		topDescription = new JTextArea("Wybierz\n");
+		pane.add(topDescription);
+		// yesButton = new JButton("TAK");
+		// pane.add(yesButton);
+		// noButton = new JButton("NIE");
+		// pane.add(noButton);
+
+		JButton quitButton = new JButton("Koniec");
+		quitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				System.exit(0);
+			}
+		});
+		pane.add(quitButton);
+	}
 }
